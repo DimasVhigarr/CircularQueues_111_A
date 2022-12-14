9f99e4e336e7256f89a4bd3636ac9fef236319a3
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-12-14 16:48:28 +0700

    add function queues

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -10,5 +10,12 @@ namespace CircularQueues
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+        public Queues()
+        {
+            /*initializing the values of the variables Rear and Front to -1 to indicate that the queue is initialli\y empty*/
+            FRONT = -1;
+            REAR = -1;
+        }
+
     }
 }
